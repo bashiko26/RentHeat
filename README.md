833b6678743a262428e6eb76ddab5d034dae4b26
bashiko <bashiko26@gmail.com>
2018-03-23 21:55:22 +0900

    get rent info

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # RentHeat
 Heat map of rent
+
+ToDo
+ scraper
+ 1. extract rent price from webpage
+ 2. extend station to line
+
--- scraper/scrape.py
+++ scraper/scrape.py
@@ -6,10 +6,23 @@ def get_price(url):
     html = urllib.request.urlopen(url)
     soup = BeautifulSoup(html, "html.parser")
 
+    # get title
     title = soup.title.string
+    print("title: " + title)
+    
+    # get update date
+    update_date = soup.find("p", class_="graph-updated").string
+    print("update date: " + update_date)
+    
+    # get rent list
+    rent_list = soup.find_all("tr", class_="js-graph-data")
+    for rent in rent_list:
+        floor = rent.find("td").string
+        price_i = float(rent.find("span").string)*10
+        price = str(price_i)
 
-    print(title)
-        
+        print(floor + "\t: " + price + " K yen")
+
 if __name__ == '__main__':
-    url = 'https://suumo.jp/chintai/soba/chiba/ek_29360/'
-    get_price(url)
+    url_nishifuna = 'https://suumo.jp/chintai/soba/chiba/ek_29360/'
+    get_price(url_nishifuna)
