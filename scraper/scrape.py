d62315f8b30e8e5c026e5b001f8a08e4855a21e8
bashiko <bashiko26@gmail.com>
2018-03-29 22:06:13 +0900

    文字コード指定 Todo: 路線から駅リスト取得

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,4 +1,6 @@
-# coding: UTF-8
+# coding: utf-8
+
+import sys
 import urllib.request
 from bs4 import BeautifulSoup
 
@@ -6,25 +8,33 @@ class Scrape:
  
     def get_price(self, url):
         html = urllib.request.urlopen(url)
-        soup = BeautifulSoup(html, "html.parser")
+        soup = BeautifulSoup(html, 'html.parser')
         
         # get title
         title = soup.title.string
-        print("title: " + title)
+        print('title: ' + title)
         
         # get update date
-        update_date = soup.find("p", class_="graph-updated").string
-        print("update date: " + update_date)
+        update_date = soup.find('p', class_='graph-updated').string
+        print('更新日: ' + update_date)
         
         # get rent list
-        rent_list = soup.find_all("tr", class_="js-graph-data")
+        rent_list = soup.find_all('tr', class_='js-graph-data')
         for rent in rent_list:
-            floor = rent.find("td").string
-            price_i = float(rent.find("span").string)*10
-            price = str(price_i)
-            
-            print(floor + "\t: " + price + " K yen")
+            floor = rent.find('td').string
+            price = rent.find('span').string
+            unit = '万円'
+            print(floor + '\t: ' + price + unit)
 
+    def get_stations(url):
+        html = urllib.request.urlopen(url)
+        soup = BeautifulSoup(html, "html.parser")
+
+        # get title
+
+        # get line
+
+        # return station list
 
 if __name__ == '__main__':
     url_nishifuna = 'https://suumo.jp/chintai/soba/chiba/ek_29360/'
